01fd05dddef25196837ad107e56173ec7e8ff66f
Ada Atualizações <57003017+adaatualizacao@users.noreply.github.com>
2025-03-18 01:13:42 +0000

    Dados carta1 desafio super trunfo

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>  // Inclua essa biblioteca para usar strcspn()
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
@@ -17,6 +18,65 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    // Iniciando com a declaração de variáveis.
+    char estado;
+    char codCarta;
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontosTuristicos;
+
+
+    //Criando as mensagens e capturando os dados inseridos pelo usuário
+
+    //pegando Letra do estado
+    printf("Digite o código do Estado: \n" );
+    scanf("%c", &estado);
+
+    //pegando codigo da carta
+    printf("Digite o código da carta: \n");
+    scanf(" %c", &codCarta);
+
+    // Limpa o buffer do stdin antes de fgets()
+    while (getchar() != '\n');
+
+    //pegando o nome da cidade
+    printf("Favor digite o nome da cidade: \n");
+    fgets(cidade, 50, stdin);
+
+    // Remover a quebra de linha
+    cidade[strcspn(cidade, "\n")] = '\0';
+
+    //pegando população
+    printf("Digite o valor da população: \n");
+    scanf("%d", &populacao);
+
+    //pegando área em km2
+    printf("Digite o Km2 da Area: \n");
+    scanf("%f", &area);
+
+    // Pegando o valor do PIB da cidade
+    printf("Favor digite o PIB desta cidade: \n");
+    scanf("%f", &pib);
+
+    //Pegando quantidade de pontos turísticos
+    printf("Digite a quantidade de pontos turísticos desta cidade: \n");
+    scanf("%d", &pontosTuristicos);
+
+    // Exibindo os dados formatados corretamente
+    printf("\nCarta 1\n");
+    printf("Estado: %c\n", estado);
+    printf("Código: %c0%c  \n", estado, codCarta);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f\n", area);
+    printf("PIB: %.2f\n", pib);
+    printf("Número de Pontos Turísticos: %d\n", pontosTuristicos);
+
+
+
 
     return 0;
 }
